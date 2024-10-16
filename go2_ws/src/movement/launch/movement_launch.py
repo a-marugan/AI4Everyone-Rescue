72cbca8a8e9fa823b5bc2b32794720005acaf375
OscarPong <z5376231@ad.unsw.edu.au>
2024-10-16 20:55:06 +1100

    Add initial motion packages

---
+++ go2_ws/src/movement/launch/movement_launch.py
@@ -0,0 +1,14 @@
+import launch
+
+from launch_ros.actions import Node
+    
+def generate_launch_description():
+
+    return launch.LaunchDescription([
+        Node(
+            package="movement",
+            executable="move_to_location",
+            name="move_to_location",
+            output='screen'
+        )
+    ])
