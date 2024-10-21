419b66b63bfae77fa8b6be032f25aa12b6357894
OscarPong <z5376231@ad.unsw.edu.au>
2024-10-21 12:16:42 +1100

    Test motion packes

--- go2_ws/src/movement/src/move_to_location.cpp
+++ go2_ws/src/movement/src/move_to_location.cpp
@@ -8,7 +8,8 @@
 #include "common/ros2_sport_client.h"
 #include "interfaces/srv/position.hpp"
 
-static const float dt_ = 0.5;     // Time step in seconds
+static const float dt = 0.5;     // Time step in seconds
+static const float maxSpeed = 0.3;
 
 class MoveToLocation : public rclcpp::Node
 {
@@ -22,26 +23,24 @@ public:
         // Create the position service 
         positionSrv_ = create_service<interfaces::srv::Position>("position", std::bind(&MoveToLocation::serviceCallback, this, std::placeholders::_1, std::placeholders::_2));
         // Create a timer when called
-        timer_ = this->create_wall_timer(std::chrono::milliseconds(static_cast<int>(dt_ * 1000)), std::bind(&MoveToLocation::timerCallback, this));
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(static_cast<int>(dt * 1000)), std::bind(&MoveToLocation::timerCallback, this));
 
         // Initialise variables
         startTimer_ = false;
-
-        //posArray_.push_back(Position(-0.5, 0));
-        // Position pos;
-        // pos.x = 1.3;
-        // pos.y = 0;
-        // pos.yaw = 0;
-
-        // moveToPosition(pos);
-    };
+        currentX_ = 0;
+        currentY_ = 0;
+        currentYaw_ = 0;
+        targetX_ = currentX_;
+        targetY_ = currentY_;
+        targetYaw_ = currentYaw_;
+    }
 
 private:
 
     void serviceCallback(const std::shared_ptr<interfaces::srv::Position::Request> request, std::shared_ptr<interfaces::srv::Position::Response> response) 
     {
-        targetX_ = request->x + currentX_;
-        targetY_ = request->y + currentY_;
+        targetX_ = request->x;
+        targetY_ = request->y;
         targetYaw_ = std::atan2(targetY_ - currentY_, targetX_ - currentX_);
 
         startTimer_ = true;
@@ -52,84 +51,55 @@ private:
     {
         // If service not called, do nothing
         if (startTimer_ == false) return;
-        // If target reached, do nothing
-        if (abs(targetX_ - currentX_) <= 0.1 && abs(targetY_ - currentY_) <= 0.1) return;
 
-        float speed = 0.3;
-        float yaw = std::atan2(targetY_ - currentY_, targetX_ - currentX_);
-        vx_ = speed * std::cos(yaw);
-        vy_ = speed * std::sin(yaw);
+        // Calculate error
+        float yawError = targetYaw_ - currentYaw_;
+        while (yawError > M_PI) yawError -= 2 * M_PI;
+        while (yawError < -M_PI) yawError += 2 * M_PI;
+        float xError = targetX_ - currentX_;
+        float yError = targetY_ - currentY_;
+        vx_ = 0;
+        vy_ = 0;
         vyaw_ = 0;
 
-        // vx_ = computePIDcontrol(currentX_, targetX_, 0.1, 0, 0);
-        // vy_ = computePIDcontrol(currentY_, targetY_, 0.1, 0, 0);
-        //vyaw_ = computePIDcontrol(currentYaw_, targetYaw_, 0.1, 0, 0);
+        if (abs(yawError) > 0.1) {
+            // If yaw not reached
+            if (yawError > 0) {
+                vyaw_ = maxSpeed;
+            }
+            else {
+                vyaw_ = -maxSpeed;
+            }
+        }
+        else if (abs(xError) > 0.1 || abs(yError) > 0.1) {
+            // else if target not reached
+            vx_ = maxSpeed;
+        }
 
         sportClient_.Move(reqMsg_, vx_, vy_, vyaw_);
         reqPuber_->publish(reqMsg_);
+        
     }
 
-    // TODO: Implement PID control on position
-    float computePIDcontrol(float input, float setpoint, float kp, float ki, float kd) 
-    {
-        error_ = setpoint - input;
-        integral_ = integral_ + error_ * dt_;
-        derivative_ = (error_ - prevError_) / dt_;
-        float output = kp * error_ + ki * integral_ + kd * derivative_;
-        prevError_ = error_;
+    // float computePIDcontrol(float input, float setpoint, float kp, float ki, float kd) 
+    // {
+    //     error_ = setpoint - input;
+    //     integral_ = integral_ + error_ * dt;
+    //     derivative_ = (error_ - prevError_) / dt;
+    //     float output = kp * error_ + ki * integral_ + kd * derivative_;
+    //     prevError_ = error_;
 
-        // temp speed cap
-        float maxSpeed = 0.3;
-        if (output > maxSpeed) output = maxSpeed;
-        if (output < maxSpeed) output = -maxSpeed;
+    //     // temp speed cap
+    //     float maxSpeed = 0.3;
+    //     if (output > maxSpeed) output = maxSpeed;
+    //     if (output < maxSpeed) output = -maxSpeed;
 
-        return output;
-    }
-
-    // void moveToPosition(Position pos) {
-    //     int i = 0;
-    //     std::vector<PathPoint> path;
-    //     // for (Position& pos : posArray_) {
-    //     path.clear();
-    //     if (i > 0) {
-    //         x0_ = posArray_[i - 1].x;
-    //         y0_ = posArray_[i - 1].y;
-    //         yaw0_ = posArray_[i - 1].yaw;
-    //     }
-
-    //     double gradient;
-
-    //     if (x0_ != pos.x) {
-    //         gradient = (y0_ - pos.y) / (x0_ - pos.x);
-    //     } else {
-    //         gradient = 1; 
-    //         //
-    //     }
-
-    //     double intercept = y0_ - gradient * x0_;
-    //     double horizontal_diff = pos.x - x0_;
-    //     // double vertical_diff = pos.y - y0_;
-
-    //     for (int i = 0; i < 30; ++i) {
-    //         PathPoint tempPathPoint;
-    //         tempPathPoint.x = i * horizontal_diff + x0_;
-    //         tempPathPoint.y = gradient * tempPathPoint.x + intercept;
-    //         tempPathPoint.yaw = 0;
-    //         tempPathPoint.vx = 0.2;
-    //         tempPathPoint.vy = 0.2;
-    //         tempPathPoint.vyaw = 0;
-    //         path.push_back({tempPathPoint});
-    //     }
-
-    //     while (!(abs(x_ - pos.x) < 0.2 && abs(y_ - pos.y) < 0.2)) {
-    //         sportClient_.TrajectoryFollow(reqMsg_, path);
-    //         reqPuber_->publish(reqMsg_);
-    //     }
+    //     return output;
     // }
 
     void stateCallback(unitree_go::msg::SportModeState::SharedPtr data)
     {
-        // Get current position of robot
+        // Get current real position of robot
         currentX_ = data->position[0];
         currentY_ = data->position[1];
         currentYaw_ = data->imu_state.rpy[2];
@@ -139,7 +109,7 @@ private:
     rclcpp::Subscription<unitree_go::msg::SportModeState>::SharedPtr stateSuber_;
     rclcpp::Publisher<unitree_api::msg::Request>::SharedPtr reqPuber_;
     rclcpp::Service<interfaces::srv::Position>::SharedPtr positionSrv_;
-    rclcpp::TimerBase::SharedPtr timer_; 
+    rclcpp::TimerBase::SharedPtr timer_;
 
     unitree_api::msg::Request reqMsg_;
     SportClient sportClient_;
